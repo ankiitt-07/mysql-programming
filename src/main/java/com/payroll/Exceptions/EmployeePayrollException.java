7337a8ca4834954a81998920bca5371da55112f6
ankiitt-07 <ak077636@gmail.com>
2025-04-16 13:33:04 +0530

    [Ankit] Add :JDBC UC 3-4 Implemented updateEmployeeSalary through jdbc

---
+++ src/main/java/com/payroll/Exceptions/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package com.payroll.Exceptions;
+
+public class EmployeePayrollException extends Exception {
+    public EmployeePayrollException(String message) {
+        super(message);
+    }
+}
