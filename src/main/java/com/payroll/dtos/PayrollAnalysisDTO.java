ddd846317d393df4f7c8305ebfa37d4a4b65cbee
ankiitt-07 <ak077636@gmail.com>
2025-04-16 13:30:18 +0530

    [Ankit] Add: JDBC UC6: Implemented getPayrollAnalysisByGender and added JUnit tests

---
+++ src/main/java/com/payroll/dtos/PayrollAnalysisDTO.java
@@ -0,0 +1,29 @@
+package com.payroll.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PayrollAnalysisDTO {
+    private String gender;
+    private double totalSalary;
+    private double avgSalary;
+    private double minSalary;
+    private double maxSalary;
+    private int employeeCount;
+
+    public PayrollAnalysisDTO(String gender, double totalSalary, double avgSalary, double minSalary, double maxSalary, int employeeCount) {
+        this.gender = gender;
+        this.totalSalary = totalSalary;
+        this.avgSalary = avgSalary;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.employeeCount = employeeCount;
+    }
+
+    @Override
+    public String toString() {
+        return "[ Gender: " + gender + ", Total Salary: " + totalSalary + ", Avg Salary: " + avgSalary + ", Min Salary: " + minSalary + ", Max Salary: " + maxSalary + ", Employee Count: " + employeeCount + " ]";
+    }
+}
